3213d6566fb6e1d677c2c036100816e728685326
yoihenken <bagusbs226@gmail.com>
2022-07-24 20:59:47 +0700

    edit be

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,6 @@
 export default {
-    BASE_URL: "http://localhost:5000/",
+    // BASE_URL: "http://localhost:5000/",
+    BASE_URL: "http://2d31-182-1-70-72.ngrok.io",
     API_PREFIX: "api/",
     STORAGE_KEY: {
         TOKEN: "__tang__",
